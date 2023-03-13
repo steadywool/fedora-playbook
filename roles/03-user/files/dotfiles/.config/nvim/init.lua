ba2c80ed80dba4aaefb05d9d25110e23ba5bc91e
kaniville <60816131+Kaniville@users.noreply.github.com>
2023-03-13 23:46:02 +0100

    Added playbook

---
+++ roles/03-user/files/dotfiles/.config/nvim/init.lua
@@ -0,0 +1,56 @@
+-- Leader key
+vim.g.mapleader = ';'
+
+-- Autocmds, functions & keymaps
+require('autocmds')
+require('functions')
+require('keymaps')
+
+-- Plugins
+require('plugins')
+require_plugins()
+
+-- Files
+vim.opt.swapfile = true
+vim.opt.undofile = true
+
+-- Providers
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+
+-- Clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- Mouse usage
+vim.opt.mouse = 'a'
+
+-- Lines
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+
+-- True color
+vim.opt.termguicolors = true
+
+-- Auto indentation
+vim.opt.autoindent = true
+
+-- Folding
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldlevel = 99
+vim.opt.foldenable = false
+
+-- Tabs & spaces
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '▶ ', trail = '•', nbsp = '␣' }
+
+-- Statusline
+vim.opt.statusline = '%#search# %n %#statusline# %F %r%m %= %w%y %#incsearch# %l:%c %p%% '
---
+++ roles/03-user/files/dotfiles/.zshenv
@@ -0,0 +1,34 @@
+# ZSH
+ZSH_CONFIG_DIR=${HOME}/.config/zsh
+ZSH_PLUGIN_DIR=${HOME}/.config/zsh/plugins
+
+# Vi Mode
+KEYTIMEOUT=1
+
+# History
+HISTFILE=${HOME}/.zhistory
+HISTSIZE=5000
+SAVEHIST=5000
+
+# Prompt
+PS1='%B%(!.%F{red}.%F{green})%n@%m%f %F{blue}%(4~|.../%3~|%~)%f ❯ %b'
+RPS1='%B${vcs_info_msg_0_} %F{red}%(?..[%? ⨯])%f%b%E'
+
+# Variables
+export EDITOR=nvim
+export VISUAL=nvim
+export PAGER=less
+
+# GPG Agent
+export GPG_TTY=$(tty)
+
+# PATH
+export PATH=${PATH}:${HOME}/.local/bin:${HOME}/.nix-profile/bin
+export XDG_DATA_DIRS=${XDG_DATA_DIRS}:${HOME}/.nix-profile/share
+export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
+
+# Wayland / Sway
+export XDG_CURRENT_DESKTOP=sway
+export _JAVA_AWT_WM_NONREPARENTING=1
+export MOZ_ENABLE_WAYLAND=1
+export LIBSEAT_BACKEND=logind
