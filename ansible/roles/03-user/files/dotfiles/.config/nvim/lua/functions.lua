afb3e4eda403da6b6adaa31e1500b6c013c707bb
kaniville <60816131+Kaniville@users.noreply.github.com>
2023-03-14 21:17:22 +0100

    Removed the 03-user role, conflict with the 04-user role

--- ansible/roles/03-user/files/dotfiles/.config/nvim/lua/functions.lua
+++
@@ -1,6 +0,0 @@
--- Load all plugins under /lua/plugins
-function require_plugins()
-	for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath('config') .. '/lua/plugins')) do
-		require('plugins.' .. file:gsub('%.lua$', ''))
-	end
-end
