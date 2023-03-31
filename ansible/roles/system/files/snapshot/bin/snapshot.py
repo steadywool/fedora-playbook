87eebba4a65601327024558f76ecaaac896c5b0a
kaniville <60816131+Kaniville@users.noreply.github.com>
2023-03-31 22:00:03 +0200

    Updated syntax

--- ansible/roles/system/files/snapshot/bin/snapshot.py
+++ ansible/roles/system/files/snapshot/bin/snapshot.py
@@ -1,22 +1,23 @@
 #!/bin/python3
 
-import datetime # Allows us to have the timestamp
-import subprocess # Allows us to use the btrfs command
-import configparser # Allows us to read in a configuration file
-import ast # Allows us to make our values from the configuration file usable
-import glob # Sorts the contents of the snapshot directory
-import os # //
-import sys # Exit the program if something is wrong
-import argparse # Pass arguments to the script
+import datetime  # Allows us to have the timestamp
+import subprocess  # Allows us to use the btrfs command
+import configparser  # Allows us to read in a configuration file
+import ast  # Allows us to make our values from the configuration file usable
+import glob  # Sorts the contents of the snapshot directory
+import os  # //
+import sys  # Exit the program if something is wrong
+import argparse  # Pass arguments to the script
 
 # Only execute this script as root
-if not os.geteuid() == 0: sys.exit("Execute this script as root !")
+if not os.geteuid() == 0:
+    sys.exit("Execute this script as root !")
 
 # Argument parser setup
 argument_parser = argparse.ArgumentParser()
-argument_parser.add_argument('-i', '--info', default='default', help='Add additionnal information', required=False)
-argument_parser.add_argument('-c', '--create', action="store_true", help='Create snapshots')
-argument_parser.add_argument('-d', '--delete', action="store_true", help='Remove old snapshots')
+argument_parser.add_argument('-i', '--info', default='default', help='add additionnal information', required=False)
+argument_parser.add_argument('-c', '--create', action="store_true", help='create snapshots')
+argument_parser.add_argument('-d', '--delete', action="store_true", help='remove old snapshots')
 option = argument_parser.parse_args()
 
 try:
@@ -42,6 +43,7 @@ try:
 except:
     sys.exit("Main items not found !")
 
+
 # Create snapshots
 def create_snapshot():
     for subvol_name in subvolume_dictionary:
@@ -51,24 +53,31 @@ def create_snapshot():
         subvol_information = option.info
 
         try:
-            subprocess.run(['btrfs', 'subvolume', 'snapshot', '-r', f'{subvol_directory}', f'{snapshot_dir}/{current_date}_{subvol_information}_{subvol_name}'], check=True)
+            subprocess.run(['btrfs', 'subvolume', 'snapshot', '-r',
+                           f'{subvol_directory}', f'{snapshot_dir}/{current_date}_{subvol_information}_{subvol_name}'], check=True)
         except:
             sys.exit("Creation of snapshots failed.")
+
 
 # Delete old snapshots
 def delete_snapshot():
     for subvol_name in subvolume_dictionary:
 
         # Sort every snapshots from the directory by date of creation
-        old_snapshots = sorted(glob.glob(os.path.join(snapshot_dir, f'*{subvol_name}')), key=os.path.getmtime)
+        old_snapshots = sorted(glob.glob(os.path.join(
+            snapshot_dir, f'*{subvol_name}')), key=os.path.getmtime)
 
         try:
             # Here I slice everything except the last snapshots
             for snap in old_snapshots[:-keep_snapshots]:
-                subprocess.run(['btrfs', 'subvolume', 'delete', f'{snap}'], check=True)
+                subprocess.run(
+                    ['btrfs', 'subvolume', 'delete', f'{snap}'], check=True)
         except:
             sys.exit("Deletion of snapshots failed.")
 
+
 # Assigns a function to the options
-if option.create: create_snapshot()
-if option.delete: delete_snapshot()
+if option.create:
+    create_snapshot()
+if option.delete:
+    delete_snapshot()
