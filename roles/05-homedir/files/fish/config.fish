1e52dc05e09f8dd27a48f33c58e9b6cf9863920d
kaniville <60816131+kaniville@users.noreply.github.com>
2023-08-27 20:16:47 +0200

    Use VSCode instead of Neovim

---
+++ roles/05-homedir/files/fish/config.fish
@@ -0,0 +1,28 @@
+# VARIABLES
+## PATH variable
+set -gx PATH $HOME/.local/bin $HOME/.local/share/flatpak/exports/bin $PATH
+
+## XDG variables
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_DATA_HOME $HOME/.local/share
+
+## Editor variables
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx PAGER less
+
+## Wayland variables
+set -x MOZ_ENABLE_WAYLAND 1
+
+#-------------------------------------------------------------------
+# INTERACTIVE SHELL
+if status is-interactive
+    ## Remove fish greeting
+    set fish_greeting
+
+    ## Always source aliases
+    for i in (ls $__fish_config_dir/aliases/)
+        source $__fish_config_dir/aliases/$i
+    end
+end
