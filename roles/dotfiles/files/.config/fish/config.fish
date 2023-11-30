af23d37eec2f051ee4a5b8c3cdb0cb5c073ad838
steadywool <60816131+steadywool@users.noreply.github.com>
2023-11-30 17:28:24 +0100

    Use vim-minimal as default CLI editor

--- roles/dotfiles/files/.config/fish/config.fish
+++ roles/dotfiles/files/.config/fish/config.fish
@@ -6,6 +6,11 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_DATA_HOME $HOME/.local/share
 
+## Editor variables
+set -gx EDITOR vi
+set -gx VISUAL vi
+set -gx PAGER less
+
 # Interactive
 if status is-interactive
     # Remove fish greeting
