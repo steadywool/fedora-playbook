0f47020fa7d7e934b0adbb8dd1b7f2f6fccb8caf
Marchand Léo <60816131+steadywool@users.noreply.github.com>
2024-01-02 16:20:13 +0100

    Updated Fish

--- roles/dotfiles/files/.config/fish/config.fish
+++ roles/dotfiles/files/.config/fish/config.fish
@@ -15,9 +15,4 @@ set -gx PAGER less
 if status is-interactive
     # Remove fish greeting
     set fish_greeting
-
-    # Always source aliases
-    for i in (ls $__fish_config_dir/aliases/)
-        source $__fish_config_dir/aliases/$i
-    end
 end
--- roles/dotfiles/files/.config/fish/functions/nmwall.fish
+++ roles/dotfiles/files/.config/fish/functions/nmwall.fish
@@ -1,3 +1,3 @@
-function nmwall -a ssid -a zone
+function nmwall --argument ssid zone
     command nmcli connection modify $ssid connection.zone $zone
 end
